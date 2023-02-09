70cf8591f4a0dce5d191e8495339cf85521e42a4
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-09 18:55:04 +0100

    axios added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import axios from "axios";
 
-function App() {
+ const BASE_URL = "https://api.pexels.com/v1/search?query=space";
+
+ function App() {
+
+const response = axios(
+  { baseURL: BASE_URL,
+    headers: { Authorization: process.env.REACT_APP_API_KEY}
+  })
+  .then(response => console.log(response.data))
+  console.log(response.data)
+
   return (
     <div>
+      <h1 className="header"> Memo Game</h1>
     </div>
   );
 }
