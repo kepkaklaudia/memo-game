de737da12a44b212c0431f6154db85d9cd20e45c
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-09 20:34:19 +0100

    URLSearchParams added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 
- const BASE_URL = "https://api.pexels.com/v1/search?query=space";
+const getRandomPage = () => Math.floor(Math.random() * 10) + 1;
 
- function App() {
+function App() {
 
-const response = axios(
-  { baseURL: BASE_URL,
-    headers: { Authorization: process.env.REACT_APP_API_KEY}
-  })
-  .then(response => console.log(response.data))
+  const buildURL = () => {
+    let url = new URL("https://api.pexels.com/v1/search");
+
+    url.search = new URLSearchParams({
+      query: "space", //tochange
+      orientation: "square",
+      size: "small",
+      per_page: 2, //tochange
+      page: getRandomPage(),
+    })
+    return url;
+  };
+
+  const response = axios(
+    {
+      url: buildURL(),
+      headers: { 
+        Authorization: process.env.REACT_APP_API_KEY 
+      }
+    })
+    .then(response => console.log(response.data))
   console.log(response.data)
 
   return (
