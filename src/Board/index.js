19d8cf0dc9dfeccda001e8fe75496b2aa92dc433
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-11 00:17:46 +0100

    useLogic added

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useImages } from "../useImages"
 import { Loader } from "./Loader";
+import { useLogic } from "./useLogic";
 
 export const Board = ({ gameSettings }) => {
   const [isLoading, setIsLoading] = useState(true);
   const images = useImages(gameSettings);
+  const cards = useLogic(images);
 
   useEffect(() => {
     if (images.length > 0) setIsLoading(false);
