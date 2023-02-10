e11c2a07c53749faa7160537d112595f087fa3f9
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 10:23:05 +0100

    RadioBoxWrapper changed to BoxWrapper

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -11,7 +11,7 @@ export const Heading = styled.h2`
   text-align: center;
 `;
 
-export const RadioBoxWrapper = styled.div`
+export const BoxWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
   background-color: rgba(255,255,255,0.5);
