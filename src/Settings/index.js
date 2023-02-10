7d4e708d13985cb96533addbb79e799a57218c5d
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 10:45:27 +0100

    Counter added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,15 +1,21 @@
-import { categories } from "./constants";
+import { categories, initialCardsCount } from "./constants";
 import { RadioBox } from "./RadioBox";
 import { Wrapper, Heading, BoxWrapper } from "./styled";
 import { useState } from "react";
+import { Counter } from "./Counter";
 
 export const Settings = () => {
   const [category, setCategory] = useState(categories[0]);
+  const [cardsCount, setCardsCount] = useState(initialCardsCount);
 
   return (
     <Wrapper>
-      <Heading>Settings</Heading>
-      <h3>Category: </h3>
+      <Heading>
+        Settings
+      </Heading>
+      <h3>
+        Category:
+      </h3>
       <BoxWrapper>
         {categories.map(item => (
           <RadioBox
@@ -19,6 +25,15 @@ export const Settings = () => {
             onChange={event => setCategory(event.target.value)}
           />
         ))}
+      </BoxWrapper>
+      <h3>
+        Amount of cards:
+      </h3>
+      <BoxWrapper>
+        <Counter
+          cardsCount={cardsCount}
+          onClick={setCardsCount}
+        />
       </BoxWrapper>
     </Wrapper>
   )
