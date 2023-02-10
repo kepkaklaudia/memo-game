4205a1fb257f97bbe9457dd6461be22e66246fd5
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 13:28:47 +0100

    responsive added

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -1,15 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 0 auto;
   display: grid;
   border-radius: 25px;
   padding: 10px;
-  background-color: rgba(255,255,255,0.5);
+  background-color: rgba(255,255,255,0.5); 
+  max-width: 700px; 
 `;
 
 export const Heading = styled.h2`
   text-align: center;
+  margin-bottom: 0px;
 `;
 
 export const BoxWrapper = styled.div`
@@ -20,15 +22,23 @@ export const BoxWrapper = styled.div`
   padding: 10px;
   margin: 16px;
   border-radius: 5px;
-  box-shadow: 0 0 5px 1px rgba(0,0,0,0.5);
-  `;
+  box-shadow: 0 0 5px 1px rgba(77, 77, 77, 0.5);
 
-  export const Button = styled.button`
+  ${({ responsive }) => responsive && css`
+    @media (max-width: 485px) {
+      display: grid;
+      grid-gap: 10px;
+    }
+  `}
+`;
+
+export const Button = styled.button`
   width: 120px;
   justify-self: center;
   padding: 10px;
   margin: 16px;
-  border: 1px solid;
+  border: none;
   border-radius: 10px;
+  box-shadow: 0 0 5px 1px rgba(77, 77, 77, 0.5);
   cursor: pointer;
-  `;
+`;
