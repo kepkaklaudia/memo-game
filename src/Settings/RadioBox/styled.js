3cac5ec22c934150c883a56bdfa0edb3aa91281c
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 12:43:16 +0100

    cursor pointer added, scale changed

--- src/Settings/RadioBox/styled.js
+++ src/Settings/RadioBox/styled.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const Input = styled.input`
   accent-color: rgb(0, 140, 150);
-  scale: 1.1;
-  margin-right: 8px;
+  scale: 1.3;
+  margin-right: 10px;
+  cursor: pointer;
 
   &:focus-visible {
     outline: 1px solid;
