ac82d5e7816d812b86a17561961f2fd2e88bcd97
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 10:23:25 +0100

    isChecked added

--- src/Settings/RadioBox/index.js
+++ src/Settings/RadioBox/index.js
@@ -1,8 +1,19 @@
 import { Input } from "./styled";
 
-export const RadioBox = ({ name }) => (
+export const RadioBox = ({ name, selectedItem, onChange }) => {
+  const isChecked = name === selectedItem;
+
+  return (
     <label>
-      <Input type="radio" name={name} id={name} value={name} />
+      <Input
+        type="radio"
+        name={name}
+        id={name}
+        value={name}
+        checked={isChecked}
+        onChange={onChange}
+      />
       {name}
     </label>
-);
+  );
+}
