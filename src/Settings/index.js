a18c97efdf6cef98094f766b75077b14fdd5657e
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 10:22:43 +0100

    setCategory added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,15 +1,25 @@
 import { categories } from "./constants";
 import { RadioBox } from "./RadioBox";
-import { Wrapper, Heading, RadioBoxWrapper } from "./styled";
+import { Wrapper, Heading, BoxWrapper } from "./styled";
+import { useState } from "react";
 
-export const Settings = () => (
-  <Wrapper>
-    <Heading>Settings</Heading>
-    <h3>Category: </h3>
-    <RadioBoxWrapper>
-      {categories.map(category => (
-        <RadioBox key={category} name={category} />
-      ))}
-    </RadioBoxWrapper>
-  </Wrapper>
-);
+export const Settings = () => {
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Wrapper>
+      <Heading>Settings</Heading>
+      <h3>Category: </h3>
+      <BoxWrapper>
+        {categories.map(item => (
+          <RadioBox
+            key={item}
+            name={item}
+            selectedItem={category}
+            onChange={event => setCategory(event.target.value)}
+          />
+        ))}
+      </BoxWrapper>
+    </Wrapper>
+  )
+};
