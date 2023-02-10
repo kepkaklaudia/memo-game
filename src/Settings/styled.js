7616013a6f05d69c3267a81b1c76ee98a8c94917
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 13:29:08 +0100

    button transition added

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -41,4 +41,18 @@ export const Button = styled.button`
   border-radius: 10px;
   box-shadow: 0 0 5px 1px rgba(77, 77, 77, 0.5);
   cursor: pointer;
+  transition: all 0.25s ease-in-out;
+  font-family: 'Permanent Marker', cursive;
+  font-size: 25px; 
+
+  &:hover {
+    box-shadow: 0 0 5px 1px rgba(245,245,245, 1);
+    scale: 1.1;
+  }
+  &:active {
+    scale: 1.05;
+  }
+  &:focus-visible {
+    outline: 1px solid;
+  } 
 `;
