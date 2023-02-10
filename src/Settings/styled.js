b89bf9ef4b1d9ff4f53b1eabe94d42c51313660a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 12:43:01 +0100

    button added, display grid added

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 0 auto;
+  display: grid;
   border-radius: 25px;
   padding: 10px;
   background-color: rgba(255,255,255,0.5);
@@ -19,4 +20,15 @@ export const BoxWrapper = styled.div`
   padding: 10px;
   margin: 16px;
   border-radius: 5px;
+  box-shadow: 0 0 5px 1px rgba(0,0,0,0.5);
+  `;
+
+  export const Button = styled.button`
+  width: 120px;
+  justify-self: center;
+  padding: 10px;
+  margin: 16px;
+  border: 1px solid;
+  border-radius: 10px;
+  cursor: pointer;
   `;
