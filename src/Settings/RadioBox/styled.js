99649b2453a5f5dd394ec90a581dfe3c8617c322
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 12:16:35 +0100

    focus-visible added

--- src/Settings/RadioBox/styled.js
+++ src/Settings/RadioBox/styled.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-  accent-color: rgba(0,140,150,1);
+  accent-color: rgb(0, 140, 150);
   scale: 1.1;
   margin-right: 8px;
+
+  &:focus-visible {
+    outline: 1px solid;
+  }
 `;
