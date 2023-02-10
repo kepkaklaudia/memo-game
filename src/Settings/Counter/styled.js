4390c450b45d81246710ea3a2929e2c0c5232965
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 12:16:19 +0100

    Arrow, Button added

---
+++ src/Settings/Counter/styled.js
@@ -0,0 +1,48 @@
+import styled, { css } from "styled-components";
+
+export const Button = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  &:focus-visible {
+    outline: 1.2px solid;
+  }
+
+  &:active {
+    scale: 1.1;  
+  }
+`;
+
+export const Arrow = styled.span`
+  display: block;
+  width: 15px;
+  height: 15px;
+  border-bottom: 3px solid white;
+  border-right: 3px solid white;
+  animation: animate 1s infinite alternate;
+
+  @keyframes animate {
+    0% {
+      transform: rotate(315deg) translate(-8px, -8px);
+    }
+    100% {
+      transform: rotate(315deg) translate(2px, 2px);
+    }
+  }
+
+  ${({ decrement }) => decrement && css`
+  animation: animateD 1s infinite alternate;
+
+  @keyframes animateD {
+    0% {
+      transform: rotate(135deg) translate(-8px, -8px);
+    }
+    100% {
+      transform: rotate(135deg) translate(2px, 2px);
+    }
+  }
+  `}
+`;
