19fa6e7071eea835f45d5219cf5a2ca92281adc0
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 09:13:45 +0100

    Settings stylings added

---
+++ src/Settings/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  border-radius: 25px;
+  padding: 10px;
+  background-color: rgba(255,255,255,0.5);
+`;
+
+export const Heading = styled.h2`
+  text-align: center;
+`;
+
+export const RadioBoxWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: rgba(255,255,255,0.5);
+  padding: 10px;
+  margin:10px;
+  border-radius: 5px;
+  `;
