2e5bfc7e71587883ff7eb75351dffe4bbdcb4fe1
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 09:13:58 +0100

    Wrappers added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,12 +1,15 @@
 import { categories } from "./constants";
 import { RadioBox } from "./RadioBox";
+import { Wrapper, Heading, RadioBoxWrapper } from "./styled";
 
 export const Settings = () => (
-  <div>
-    <h2>Settings</h2>
+  <Wrapper>
+    <Heading>Settings</Heading>
     <h3>Category: </h3>
-    {categories.map(category => (
-      <RadioBox key={category} name={category} />
-    ))}
-  </div>
+    <RadioBoxWrapper>
+      {categories.map(category => (
+        <RadioBox key={category} name={category} />
+      ))}
+    </RadioBoxWrapper>
+  </Wrapper>
 );
