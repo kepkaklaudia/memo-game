9cb1eae46193b45f5abeb029516c093218962466
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 12:15:50 +0100

    align-items, margin added

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -14,8 +14,9 @@ export const Heading = styled.h2`
 export const BoxWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
   background-color: rgba(255,255,255,0.5);
   padding: 10px;
-  margin:10px;
+  margin: 16px;
   border-radius: 5px;
   `;
