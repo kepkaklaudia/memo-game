640ba5bd84b9ed75dec07dd0c3da9e47e542350a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-10 23:53:27 +0100

    Loader added

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
 import { useImages } from "../useImages"
+import { Loader } from "./Loader";
 
 export const Board = ({ gameSettings }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const images = useImages(gameSettings);
+
+  useEffect(() => {
+    if (images.length > 0) setIsLoading(false);
+  }, [images]);
+
+  return (
+    <>
+      {isLoading ? <Loader /> : <div></div>}
+    </>
+  )
 }
