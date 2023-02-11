2bfce6b8039e05c19337e1049a959d0d3e6f9787
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-11 21:33:26 +0100

    Image added

--- src/Board/Cards/index.js
+++ src/Board/Cards/index.js
@@ -1,10 +1,13 @@
+import { Image } from "./Image"
 import { Wrapper, Card, Front, Back } from "./styled"
 
 export const Cards = ({ card }) => {
-  return
-  <Wrapper>
-    <Card />
-    <Front />
-    <Back />
-  </Wrapper>
+  return (
+    <Wrapper>
+      <Card />
+      <Front />
+      <Back />
+      <Image url={card.url} />
+    </Wrapper>
+  )
 }
