4552afb2ed9a67fc45cad96d47d1d693f4277742
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-11 20:50:01 +0100

    setCards added

--- src/Board/useLogic.js
+++ src/Board/useLogic.js
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { addUniqueId, getFormedData, getPairedImages, shuffleCards } from "./gameFunctions";
 
 export const useLogic = (images) => {
+  const [cards, setCards] = useState([])
 
   const prepareCards = () => {
     const a = getFormedData(images);
     const b = getPairedImages(a);
     const c = addUniqueId(b);
     const d = shuffleCards(c);
-    console.log(d)
+    setCards(d);
   }
 
   useEffect(() => {
     if (images.length > 0) prepareCards();
   }, [images]);
 
+  return cards;
 }
