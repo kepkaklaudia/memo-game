fc697322c7a3985fa3a324016fab52ce5683af2e
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-11 21:22:08 +0100

    Components added

---
+++ src/Board/Cards/styled.js
@@ -0,0 +1,6 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.div``;
+export const Card = styled.div``;
+export const Front = styled.div``;
+export const Back = styled.div``;
