2522a7a77d6729358b8fdb030622c59eb69b6509
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 12:33:33 +0100

    Wrapper changed to CardsBox

--- src/Board/styled.js
+++ src/Board/styled.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-content: flex-start;
-    height: calc(100vh - 60px);
+export const CardsBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: flex-start;
 `;
