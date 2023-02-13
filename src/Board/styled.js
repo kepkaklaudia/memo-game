018ffd72f3f1a364f78866b45e35466dbe03f0a8
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 01:27:12 +0100

    Wrapper added

---
+++ src/Board/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: flex-start;
+    height: calc(100vh - 60px);
+`;
