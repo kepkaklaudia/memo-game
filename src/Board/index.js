b2204b3b6665f610cbc9261c4500f0d6ac2f3652
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 12:28:55 +0100

    refactoring

--- src/Board/index.js
+++ src/Board/index.js
@@ -3,24 +3,38 @@ import { useImages } from "../useImages"
 import { Cards } from "./Cards";
 import { Loader } from "./Loader";
 import { Result } from "./Result";
-import { Wrapper } from "./styled";
+import { CardsBox } from "./styled";
 import { useLogic } from "./useLogic";
 
 export const Board = ({ gameSettings, restartGame }) => {
   const [isLoading, setIsLoading] = useState(true);
   const images = useImages(gameSettings);
-  const {cards, onCardClick, isWin} = useLogic(images);
+  const { cards, onCardClick, isWin } = useLogic(images);
 
   useEffect(() => {
-    if (images.length > 0) setIsLoading(false);
+    if (images.length > 0)
+      setIsLoading(false);
   }, [images]);
 
   return (
     <>
-    {isWin && <Result restartGame={restartGame} />}
-      {isLoading ? <Loader /> : <Wrapper>
-        {cards.map(card => <Cards key={card.uniqueId} card={card} onCardClick={onCardClick} />)}
-      </Wrapper>}
+      {isWin &&
+        <Result
+          restartGame={restartGame}
+        />
+      }
+      {isLoading ?
+        <Loader /> :
+        <CardsBox>
+          {cards.map(card =>
+            <Cards
+              key={card.uniqueId}
+              card={card}
+              onCardClick={onCardClick}
+            />
+          )}
+        </CardsBox>
+      }
     </>
   )
 }
