7d8bdf2f59545bb014556d8f196a328f7e591327
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 13:52:28 +0100

    refactoring

--- src/Board/GameLogic/useLogic.js
+++ src/Board/GameLogic/useLogic.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { addUniqueId, getFormedData, getPairedImages, shuffleCards } from "./gameFunctions";
 
-export const useLogic = (images) => {
-  const [score, setScore] = useState(0)
-  const [isWin, setIsWin] = useState(false)
-  const [cards, setCards] = useState([])
-  const [visibleCards, setVisibleCards] = useState([])
+export const useLogic = images => {
+  const [score, setScore] = useState(0);
+  const [isWin, setIsWin] = useState(false);
+  const [cards, setCards] = useState([]);
+  const [visibleCards, setVisibleCards] = useState([]);
   const maxVisibleCards = 2;
 
   const prepareCards = () => {
@@ -14,52 +14,54 @@ export const useLogic = (images) => {
     const c = addUniqueId(b);
     const d = shuffleCards(c);
     setCards(d);
-  }
+  };
 
-  const flipCard = (clickedCardId) => {
+  const flipCard = clickedCardId => {
     const flippedCards = cards.map(card => {
       if (card.uniqueId === clickedCardId) {
         card.isShown = true
-      }
+      };
 
       if (card.isShown) {
         setVisibleCards(oldState => [...oldState, card.uniqueId]);
-      }
+      };
 
       return card;
-    })
+    });
 
     setCards(flippedCards);
-  }
+  };
 
   const onCardClick = clickedCardId => {
     if (visibleCards.length < maxVisibleCards) {
       flipCard(clickedCardId)
     };
-  }
+  };
 
   const updateScore = () => {
     setScore(oldScore => oldScore + 1)
-  }
+  };
 
   const checkMatch = () => {
-    const visible = cards.filter(card => visibleCards.indexOf(card.uniqueId) !== -1)
+    const visible = cards.filter(card => visibleCards.indexOf(card.uniqueId) !== -1);
     const matched = visible[0].id === visible[1].id;
 
     const updatedCards = cards.map(card => {
       if (visibleCards.indexOf(card.uniqueId) !== -1) {
-        card.isShown = false
+        card.isShown = false;
         card.isFound = matched;
-      }
+      };
+
       return card;
     });
 
     setTimeout(() => {
       setCards(updatedCards);
       setVisibleCards([]);
-      if (matched) updateScore();
+      if (matched)
+        updateScore();
     }, 1000);
-  }
+  };
 
   useEffect(() => {
     if (images.length > 0)
@@ -79,4 +81,4 @@ export const useLogic = (images) => {
   }, [score]);
 
   return { cards, onCardClick, isWin };
-}
+};
