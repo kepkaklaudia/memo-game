801cded322ee7ba68e4f8d58a8bddcea3c71ff7e
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 11:58:16 +0100

    Background changed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,26 +12,10 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    height: 100vh;
     font-family: 'Montserrat', sans-serif;
     margin: 0 20px;
-    background-color: rgba(0,140,150,1);
-    background-image: linear-gradient(41deg, rgba(0,92,96,1) 0%, rgba(0,140,150,1) 35%, rgba(0,255,132,1) 100%);
-    background-size: 300%;
-    animation: animatedGradient 20s ease infinite;
-
-    @keyframes animatedGradient { 
-      0% {
-        background-position: 0% 50%;
-      }
-      50% {
-        background-position: 100% 50%;
-      }
-      100% {
-        background-position: 0% 50%;
-      }
-    }
-  }
+    background-color: rgb(0,143,255);
+    overflow-x: hidden;
 
   h1 {
     font-family: 'Permanent Marker', cursive;
