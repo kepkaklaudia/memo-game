49f3170e00a1341f812ed221fce0763435d7da5a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 13:23:11 +0100

    Wrapper deleted

--- src/Settings/styled.js
+++ src/Settings/styled.js
@@ -1,14 +1,5 @@
 import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
-  margin: 0 auto;
-  display: grid;
-  border-radius: 25px;
-  padding: 10px;
-  background-color: rgba(255,255,255,0.5); 
-  max-width: 700px; 
-`;
-
 export const Heading = styled.h2`
   text-align: center;
   margin-bottom: 0px;
@@ -30,29 +21,4 @@ export const BoxWrapper = styled.div`
       grid-gap: 10px;
     }
   `}
-`;
-
-export const Button = styled.button`
-  width: 120px;
-  justify-self: center;
-  padding: 10px;
-  margin: 16px;
-  border: none;
-  border-radius: 10px;
-  box-shadow: 0 0 5px 1px rgba(77, 77, 77, 0.5);
-  cursor: pointer;
-  transition: all 0.25s ease-in-out;
-  font-family: 'Permanent Marker', cursive;
-  font-size: 25px; 
-
-  &:hover {
-    box-shadow: 0 0 5px 1px rgba(245,245,245, 1);
-    scale: 1.1;
-  }
-  &:active {
-    scale: 1.05;
-  }
-  &:focus-visible {
-    outline: 1px solid;
-  } 
 `;
