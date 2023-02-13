0c903f0366fe43800fc5ae048696079bd53c6c5a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 01:28:04 +0100

    Result, Wrapper added

--- src/Board/index.js
+++ src/Board/index.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useImages } from "../useImages"
 import { Cards } from "./Cards";
 import { Loader } from "./Loader";
+import { Result } from "./Result";
+import { Wrapper } from "./styled";
 import { useLogic } from "./useLogic";
 
-export const Board = ({ gameSettings }) => {
+export const Board = ({ gameSettings, restartGame }) => {
   const [isLoading, setIsLoading] = useState(true);
   const images = useImages(gameSettings);
-  const {cards, onCardClick} = useLogic(images);
+  const {cards, onCardClick, isWin} = useLogic(images);
 
   useEffect(() => {
     if (images.length > 0) setIsLoading(false);
@@ -15,9 +17,10 @@ export const Board = ({ gameSettings }) => {
 
   return (
     <>
-      {isLoading ? <Loader /> : <div>
+    {isWin && <Result restartGame={restartGame} />}
+      {isLoading ? <Loader /> : <Wrapper>
         {cards.map(card => <Cards key={card.uniqueId} card={card} onCardClick={onCardClick} />)}
-      </div>}
+      </Wrapper>}
     </>
   )
 }
