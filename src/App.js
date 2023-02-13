09d38f0389ce87fcf77cc4cc0e3006988efa3013
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-02-13 11:58:39 +0100

    Background added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Settings } from "./Settings";
 import { useState } from "react";
 import { Board } from "./Board";
+import { Background } from "./Background";
 
 function App() {
   const [gameSettings, setGameSettings] = useState()
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <>
+    <Background />
       <h1>Memo Game</h1>
       {gameSettings ?
         <Board
